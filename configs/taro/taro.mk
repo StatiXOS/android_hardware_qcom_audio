0ba5bfc2693c181fda9e2f25bf6c42fd76828ac8
Yang Li <quic_yyangli@quicinc.com>
2023-09-27 17:25:10 +0800

    HAL: add hfp lib to Enable HFP on sm8475/Palima
    Change-Id: I047bcae598b83463090b53d6edb5c8066f6cc406

--- configs/taro/taro.mk
+++ configs/taro/taro.mk
@@ -192,6 +192,7 @@ PRODUCT_PACKAGES += fai__2.0.0_0.1__3.0.0_0.0__eai_1.36_enpu2.pmd
 PRODUCT_PACKAGES += fai__2.7.2_0.0__3.0.0_0.0__eai_1.36_enpu2.pmd
 PRODUCT_PACKAGES += fai__2.7.20_0.0__3.0.0_0.0__eai_1.36_enpu2.pmd
 PRODUCT_PACKAGES += fai__3.0.0_0.0__eai_1.36_enpu2.pmd
+PRODUCT_PACKAGES += libhfp_pal
 PRODUCT_PACKAGES += libfmpal
 PRODUCT_PACKAGES += event.eai
 PRODUCT_PACKAGES += music.eai
