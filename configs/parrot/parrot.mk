0d5e13f9ea656cd71981769ef90c030bf478f1b2
Anay Wadhera <anay1018@gmail.com>
2024-02-05 22:57:51 -0500

    Remove audio.a2dp.default from parrot makefile
    Change-Id: I678243bb4b28afdc8790bb632edd15cfd07bff4f

--- configs/parrot/parrot.mk
+++ configs/parrot/parrot.mk
@@ -116,8 +116,7 @@ AUDIO_PAL += PalTest
 
 BOARD_SUPPORTS_OPENSOURCE_STHAL := true
 
-AUDIO_HARDWARE := audio.a2dp.default
-AUDIO_HARDWARE += audio.usb.default
+AUDIO_HARDWARE := audio.usb.default
 AUDIO_HARDWARE += audio.r_submix.default
 AUDIO_HARDWARE += audio.primary.$(TARGET_BOARD_PLATFORM)
 
